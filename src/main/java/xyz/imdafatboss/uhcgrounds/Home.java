920d5abec6b446a9b72fc21e540a7beceabe119c
imesh97 <imesh.nimsitha@gmail.com>
2016-06-12 10:18:46 -0400

    Initialized the FileManager.

--- src/main/java/xyz/imdafatboss/uhcgrounds/Home.java
+++ src/main/java/xyz/imdafatboss/uhcgrounds/Home.java
@@ -2,13 +2,21 @@ package xyz.imdafatboss.uhcgrounds;
 
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
+import xyz.imdafatboss.uhcgrounds.config.FileManager;
 
 public class Home extends JavaPlugin implements Listener{
+
+    FileManager fm;
 
     @Override
     public void onEnable(){
 
+        fm = new FileManager(this);
         getLogger().info("Created by imdafatboss");
+
+        fm.getConfig("config.yml").saveDefaultConfig();
+        fm.getConfig("data.yml").saveDefaultConfig();
+        fm.getConfig("messages.yml").saveDefaultConfig();
 
     }
 
