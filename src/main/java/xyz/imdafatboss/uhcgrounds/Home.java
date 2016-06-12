4f85408528815953768e2705371c70393d18560e
imesh97 <imesh.nimsitha@gmail.com>
2016-06-12 10:16:31 -0400

    New main class.

---
+++ src/main/java/xyz/imdafatboss/uhcgrounds/Home.java
@@ -0,0 +1,15 @@
+package xyz.imdafatboss.uhcgrounds;
+
+import org.bukkit.event.Listener;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Home extends JavaPlugin implements Listener{
+
+    @Override
+    public void onEnable(){
+
+        getLogger().info("Created by imdafatboss");
+
+    }
+
+}
